372bda42ef36acec1daea438c314e21b12341a2f
Jimmy Collazos <jimmy.collazos@gmail.com>
2018-05-11 09:26:18 +0200

    refactor and add tests

--- advice.js
+++ advice.js
@@ -1,51 +1,30 @@
-const BEFORE = Symbol('before')
-const AFTER = Symbol('after')
-const AROUND = Symbol('around')
-const ADVISABLE = Symbol('advisable')
-
-const factory = (sym) => (primaryFn) => (fn) => (advisable(primaryFn)[sym].push(fn), primaryFn)
-
-module.exports = advisable;
-
-module.exports.before = factory(BEFORE);
-
-module.exports.after = factory(AFTER);
-
-module.exports.around = factory(AROUND);
-
-
 /**
  * Returns an advisable function.
  * @function
  * @argument {Function} primaryFn Primary function of advice
  * @returns {Function} The advisable Function.
  */
-function advisable (primaryFn) {
-  if (primaryFn[ADVISABLE]) {
-    return primaryFn;
-  }
+module.exports = function advice(primaryFn) {
+  const before = []
+  const after = []
+  const arround = []
 
-  const gn = function advisableFn(...args) {
-    const pipeReducer = (acc, fn) => fn(...args, acc)
-    const arroundReducer = (acc, fn) => fn (primaryFn, ...args, acc)
+  const gn = function adviceFn(...args) {
+    const fireFn = (fn) => fn(...args)
+    const arroundReducer = (acc, fn) => fn(primaryFn, acc)(...args)
     let value
 
-    value = gn[BEFORE].reduce(pipeReducer, value)
-    value = gn[AROUND].length && gn[AROUND].reduce(arroundReducer, value) || primaryFn(...args)
-    value = gn[AFTER].reduce(pipeReducer, value)
+    before.forEach(fireFn)
+    value = arround.length && arround.reduce(arroundReducer, value) || primaryFn(...args)
+    after.forEach(fireFn)
 
     return value
   }
 
-  gn[ADVISABLE] = true;
-  gn[BEFORE]    = [];
-  gn[AFTER]     = [];
-  gn[AROUND]    = [];
-
-  const factory = (sym) => (fn) => (gn[BEFORE].push(fn), gn)
-  gn.before     = factory(BEFORE);
-  gn.after      = factory(AFTER);
-  gn.around     = factory(AROUND);
+  const factory = (arr) => (fn) => (arr.push(fn), gn)
+  gn.before = factory(before);
+  gn.after = factory(after);
+  gn.arround = factory(arround);
 
   return gn;
 }
--- advice.test.js
+++ advice.test.js
@@ -14,11 +14,10 @@ test('should return identity value when dont have decorators', ({plan, equal}) =
   equal(actual, expected)
 })
 
-test('should decorate argument before call primary function', ({plan, equal}) => {
-  let pFn = (obj) => obj.test
-  let obj = {test: false}
-  let expected = true
-  const actual = advice(pFn).before((v) => (obj.test = true))(obj)
+test('should enable the primary function before call it', ({plan, equal}) => {
+  const echo = (v) => echo.enabled && v;
+  let expected = 'abc'
+  let actual = advice(echo).before(() => echo.enabled = true)('abc')
 
   plan(1)
   equal(actual, expected)
@@ -28,6 +27,24 @@ test('should return fluent api when use "before" decorator', ({plan, deepEqual})
   const fn = advice(sum)
   const expected = fn.before
   const actual = fn.before(identity).before(identity).before
+
+  plan(1)
+  deepEqual(actual, expected)
+})
+
+test('should enable the primary function after call it', ({plan, equal}) => {
+  const echo = (v) => echo.enabled && v;
+  let expected = undefined
+  let actual = advice(echo).after(() => echo.enabled = true, 'ads')('abc')
+
+  plan(1)
+  equal(actual, expected)
+})
+
+test('should return fluent api when use "after" decorator', ({plan, deepEqual}) => {
+  const fn = advice(sum)
+  const expected = fn.after
+  const actual = fn.after(identity).after(identity).after
 
   plan(1)
   deepEqual(actual, expected)
